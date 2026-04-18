453cbf4214c9498170cee1832245650a51dcbd54
andreilinzing <andrei.linzing@gmail.com>
2026-04-18 18:03:49 +0300

    changes

--- network-stuff.nix
+++ network-stuff.nix
@@ -1,9 +1,19 @@
 { config, lib, pkgs, ... }:
 
 {
+  nixpkgs.overlays = [
+    (final: prev: {
+      opensnitch = prev.opensnitch.overrideAttrs (old: {
+        patches = (old.patches or []) ++ [
+          ./fix.patch
+        ];
+      });
+    })
+  ];
+
   services.opensnitch.enable = true;
 
-  home-manager.users.myuser = {
+  home-manager.users.andrei = {
     services.opensnitch-ui.enable = true;
   };
 }
